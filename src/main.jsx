bfc54361cc73eb5bdfe88aa2b023cbf6175543aa
userName <userEmail>
2025-03-26 21:40:08 +0600

    loading spinner added

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -18,6 +18,9 @@ import "react-toastify/dist/ReactToastify.css";
 import ManageEmployee from './routes/ManageEmployee.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import UpdateEmployee from './component/UpdateEmployee.jsx';
+import MyProfile from './routes/MyProfile.jsx';
+import { GrUpdate } from 'react-icons/gr';
+import UpdateProfile from './component/UpdateProfile.jsx';
 
 const router = createBrowserRouter([
   {
@@ -54,6 +57,16 @@ const router = createBrowserRouter([
       {
         path:"/dashboard/updateemployee/:id",
         element:<UpdateEmployee></UpdateEmployee>
+      },
+      {
+        path:"/dashboard/myprofile",
+        element:<MyProfile></MyProfile>
+      },
+      {
+
+        path:"/dashboard/updateprofile",
+        element:<UpdateProfile></UpdateProfile>
+
       }
     ]
   }
@@ -62,14 +75,45 @@ const router = createBrowserRouter([
 const queryClient = new QueryClient();
 
 
+const RootApp=()=>{
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
+
+
+
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ToastContainer />
+        {loading ? (
+          <div className="flex items-center justify-center h-screen">
+            <div className="flex flex-col items-center">
+              <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
+              <p className="mt-2 text-lg font-semibold text-gray-700">Loading...</p>
+            </div>
+          </div>
+        ) : (
+          <RouterProvider router={router} />
+        )}
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+};
+
+
 
 createRoot(document.getElementById('root')).render(
   
-     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-     <ToastContainer />
-     <RouterProvider router={router} />
-     </AuthProvider>
-  </QueryClientProvider>
-  
+    <RootApp></RootApp>
 )
